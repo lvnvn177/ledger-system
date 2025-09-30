6b7bb258af4f8b4755f7f826656b5ff5e43639c8
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-09-30 13:49:27 +0900

    init

---
+++ src/test/java/com/sellanding/ledger_system/LedgerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellanding.ledger_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
