b38401eb1c9141c8c700762b6441bce883a4ebca
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-09-30 14:40:24 +0900

    domain - useraccount 생성

--- src/main/java/com/sellanding/ledger_system/domain/UserAccount.java
+++ src/main/java/com/sellanding/ledger_system/domain/UserAccount.java
@@ -1,5 +1,27 @@
 package com.sellanding.ledger_system.domain;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class UserAccount {
-    
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String username;
+
+    @Column(nullable = false, name = "balance")
+    private BigDecimal balance;
+
+    @Column(name = "portifolio")
+    private List<LedgerEntry> portifolio;
 }
